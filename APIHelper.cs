94a1590aea547d99cebb7a3651d6a15bb8020033
smbdy <shadows2001@gmail.com>
2020-09-17 23:31:33 +0300

    Initial Commit

---
+++ APIHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Memenim.Core.Data;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace Memenim.Core
+{
+     struct APIEndPoints
+    {
+        public static string GENERAL_API_STRING = "http://dev.apianon.ru:3000/";
+        public static string GROUP_API_STRING = "http://dev.apianon.ru:8080";
+        public static string CHAT_API_STRING = "https://chat.apianon.ru";
+        public static string PHOTO_API_STRING = "http://fotoanon.ru";
+    }
+
+    static class APIHelper
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        /// <summary>
+        /// General solution to perform a generic request to service
+        /// </summary>
+        /// <typeparam name="T">Requested Data</typeparam>
+        /// <param name="request">String of API request</param>
+        /// <param name="requestData"></param>
+        /// <returns>Returns a response data from service</returns>
+        public static async Task<ApiResponse<T>> RequestInternal<T>(string request, object requestData)
+        {
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            var json = JsonConvert.SerializeObject(requestData);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(APIEndPoints.GENERAL_API_STRING + request, data);
+
+            string result = response.Content.ReadAsStringAsync().Result;
+            ApiResponse<T> resp = JsonConvert.DeserializeObject<ApiResponse<T>>(result);
+            return resp;
+        }
+        
+    }
+}
---
+++ PostAPI.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Memenim.Core.Data;
+
+namespace Memenim.Core
+{
+    static class PostAPI
+    {
+        public static async Task<ApiResponse<List<PostData>>> GetAllPosts()
+        {
+            var res = await APIHelper.RequestInternal<List<PostData>>("posts/get", null);
+            return res;
+        }
+    }
+}
