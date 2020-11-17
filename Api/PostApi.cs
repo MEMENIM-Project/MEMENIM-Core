11beed93e03e81fd5483e96fea59f467b1fb6450
Daerius-rs <dima.armawir@yandex.ru>
2020-11-17 07:48:13 +0300

    Features:
    - Added variants of PostApi methods with and without the token parameter (which is used for personalization of information);
    
    Refactoring:
    
    - Changed the name of some PostApi methods;

--- Api/PostApi.cs
+++ Api/PostApi.cs
@@ -7,6 +7,18 @@ namespace Memenim.Core.Api
 {
     public static class PostApi
     {
+        public static Task<ApiResponse<List<PostSchema>>> Get(
+            PostType type = PostType.Popular, int count = 20, int offset = 0)
+        {
+            var requestData = new
+            {
+                type,
+                count,
+                offset
+            };
+
+            return ApiRequestEngine.ExecuteRequestJson<List<PostSchema>>("posts/get", requestData);
+        }
         public static Task<ApiResponse<List<PostSchema>>> Get(string token,
             PostType type = PostType.Popular, int count = 20, int offset = 0)
         {
@@ -20,6 +32,29 @@ namespace Memenim.Core.Api
             return ApiRequestEngine.ExecuteRequestJson<List<PostSchema>>("posts/get", requestData, token);
         }
 
+        public static async Task<ApiResponse<PostSchema>> GetById(int id)
+        {
+            var requestData = new
+            {
+                post_ids = new[]
+                {
+                    id
+                }
+            };
+
+            var response = await ApiRequestEngine.ExecuteRequestJson<List<PostSchema>>("posts/getById", requestData)
+                .ConfigureAwait(false);
+
+            return new ApiResponse<PostSchema>
+            {
+                code = response.code,
+                data = response.data.Count != 0
+                    ? response.data[0]
+                    : null,
+                error = response.error,
+                message = response.message
+            };
+        }
         public static async Task<ApiResponse<PostSchema>> GetById(string token, int id)
         {
             var requestData = new
@@ -43,6 +78,15 @@ namespace Memenim.Core.Api
                 message = response.message
             };
         }
+        public static Task<ApiResponse<List<PostSchema>>> GetById(int[] ids)
+        {
+            var requestData = new
+            {
+                post_ids = ids
+            };
+
+            return ApiRequestEngine.ExecuteRequestJson<List<PostSchema>>("posts/getById", requestData);
+        }
         public static Task<ApiResponse<List<PostSchema>>> GetById(string token, int[] ids)
         {
             var requestData = new
@@ -53,12 +97,12 @@ namespace Memenim.Core.Api
             return ApiRequestEngine.ExecuteRequestJson<List<PostSchema>>("posts/getById", requestData, token);
         }
 
-        public static Task<ApiResponse<IdSchema>> AddPost(string token, PostSchema post)
+        public static Task<ApiResponse<IdSchema>> Add(string token, PostSchema post)
         {
             return ApiRequestEngine.ExecuteRequestJson<IdSchema>("posts/add", post, token);
         }
 
-        public static Task<ApiResponse> RemovePost(string token, int id)
+        public static Task<ApiResponse> Remove(string token, int id)
         {
             var requestData = new
             {
@@ -68,7 +112,7 @@ namespace Memenim.Core.Api
             return ApiRequestEngine.ExecuteRequestJson("posts/removePost", requestData, token);
         }
 
-        public static Task<ApiResponse> EditPost(string token, PostEditSchema schema)
+        public static Task<ApiResponse> Edit(string token, PostEditSchema schema)
         {
             return ApiRequestEngine.ExecuteRequestJson("posts/edit", schema, token);
         }
@@ -82,6 +126,15 @@ namespace Memenim.Core.Api
 
             return ApiRequestEngine.ExecuteRequestJson("posts/viewAdd", requestData);
         }
+        public static Task<ApiResponse> AddView(string token, int id)
+        {
+            var requestData = new
+            {
+                post_id = id
+            };
+
+            return ApiRequestEngine.ExecuteRequestJson("posts/viewAdd", requestData, token);
+        }
 
         public static Task<ApiResponse> AddRepost(int id)
         {
@@ -91,6 +144,15 @@ namespace Memenim.Core.Api
             };
 
             return ApiRequestEngine.ExecuteRequestJson("posts/repost", requestData);
+        }
+        public static Task<ApiResponse> AddRepost(string token, int id)
+        {
+            var requestData = new
+            {
+                post_id = id
+            };
+
+            return ApiRequestEngine.ExecuteRequestJson("posts/repost", requestData, token);
         }
 
         public static Task<ApiResponse<CountSchema>> AddLike(string token, int id)
@@ -145,6 +207,17 @@ namespace Memenim.Core.Api
             };
 
             return ApiRequestEngine.ExecuteRequestJson<List<CommentSchema>>("posts/getComments", requestData);
+        }
+        public static Task<ApiResponse<List<CommentSchema>>> GetComments(string token, int postId, int count = 20, int offset = 0)
+        {
+            var requestData = new
+            {
+                post_id = postId,
+                count,
+                offset
+            };
+
+            return ApiRequestEngine.ExecuteRequestJson<List<CommentSchema>>("posts/getComments", requestData, token);
         }
 
         public static Task<ApiResponse<IdSchema>> AddComment(string token, int postId, string text, bool? anonymous = false)
