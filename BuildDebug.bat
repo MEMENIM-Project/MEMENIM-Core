e21766107e400a4b30c1689ce7b2326679be8d6d
Daerius-rs <dima.armawir@yandex.ru>
2021-01-21 13:34:38 +0300

    Development:
    - Add Build.bat and BuildDebug.bat (for build in Release and Debug configurations)

---
+++ BuildDebug.bat
@@ -0,0 +1,8 @@
+@echo off
+for /f "usebackq delims=" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -prerelease -latest -property installationPath`) do (
+  if exist "%%i\Common7\Tools\vsdevcmd.bat" (
+    %comspec% /k " "%%i\Common7\Tools\vsdevcmd.bat" & cd "%~dp0" & echo on & dotnet build -c "Debug" "Memenim.Core.sln" & exit "
+    exit /b
+  )
+)
+exit /b 2
